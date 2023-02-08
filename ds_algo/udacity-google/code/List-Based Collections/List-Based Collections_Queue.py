836e4f51ff11b3a714042be90936a4d93c932751
anik587 <anik587@gmail.com>
2023-02-08 19:03:43 +0600

    queue in progress

--- ds_algo/udacity-google/code/List-Based Collections/List-Based Collections_Queue.py
+++ ds_algo/udacity-google/code/List-Based Collections/List-Based Collections_Queue.py
@@ -2,20 +2,30 @@
 Hint: You can use any Python list method
 you'd like! Try to write each one in as 
 few lines as possible.
-Make sure you pass the test cases too!"""
+Make sure you pass the test cases too!
+
+Enqueue : add element at the end
+Dequeue : remove element at the beginning
+Peek : Look at the head element but don't remove
+
+Deques :  Double ended queue, enqueue and dequeue in either way
+Priority Queue: assign numeric priority in queue. if the element has same priority then older elemnet will dequeue
+
+"""
 
 class Queue:
     def __init__(self, head=None):
         self.storage = [head]
 
     def enqueue(self, new_element):
+        self.storage.append(new_element)
         pass
 
     def peek(self):
-        pass 
+        return self.storage[0]
 
     def dequeue(self):
-        pass
+        return self.storage.pop(0)
     
 # Setup
 q = Queue(1)
